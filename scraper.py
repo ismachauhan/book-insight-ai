a24ad09e2e479232ba13437595ec8ac6a332ed6b
Isma Chauhan <ismachauhan8@gmail.com>
2026-04-15 17:03:22 +0530

    integrated scraper with database to store books

--- scraper.py
+++ scraper.py
@@ -1,5 +1,12 @@
+import os
+import django
 import requests
 from bs4 import BeautifulSoup
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookinsight.settings')
+django.setup()
+
+from library.models import BookData
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
@@ -10,4 +17,10 @@ books = soup.find_all("article")
 
 for book in books:
     title = book.h3.a['title']
-    print(title)
+    
+    BookData.objects.create(
+        title=title,
+        description="Sample description",
+        source_url=url
+    )
+print("Books saved successfully!")
