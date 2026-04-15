e5e0a9173cfb86ccc2b47acd1a18dc2359b02268
Isma Chauhan <ismachauhan8@gmail.com>
2026-04-15 16:46:41 +0530

    added BookData model with AI fields

--- library/models.py
+++ library/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class BookData(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField()
+    rating = models.FloatField(null=True, blank=True)
+    source_url = models.URLField()
+
+  
+    ai_summary = models.TextField(null=True, blank=True)
+    genre = models.CharField(max_length=100, null=True, blank=True)
+
+   
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
